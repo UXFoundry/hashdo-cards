3dce58b7a3d091a571ac861af085f0e477303f8a
Shaun Trennery <shaun.trennery@gmail.com>
2016-06-15 09:56:38 +0200

    E4F updates.

--- hashdo-e4f/public/sendmoney/main.js
+++ hashdo-e4f/public/sendmoney/main.js
@@ -1,16 +1,16 @@
 card.onReady = function () {
   var $card = $('#' + locals.card.id);
 
-  if (typeof __e4f === 'undefined') {
-    card.require('http://www.exchange4free.com/xandgo/lib/sendmoney.min.js?v=' + Math.round(new Date().getTime() / 1000), function () {
-      init();
-    });
-  }
-  else {
-    init();
-  }
+  if (typeof __e4f == 'undefined') {
+    card.require('http://www.exchange4free.com/xandgo/lib/sendmoney.js?v=' + Math.round(new Date().getTime() / 1000), function () {
+			init();
+		});
+	}
+	else {
+		init();
+	}
 
-  function init() {
-    __e4f.sendMoney.onReady(card, $card, locals);
-  }
-}
+	function init() {
+		__e4f.sendMoney.onReady(card, $card, locals);
+	}
+};
