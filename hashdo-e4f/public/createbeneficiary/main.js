37f0685a7f2a0d5a6eb0a12632a6c06828d92165
alan@efx-capital.com <alan@efx-capital.com>
2016-08-16 09:37:08 +0200

    Create a Beneficiary Card

---
+++ hashdo-e4f/public/createbeneficiary/main.js
@@ -0,0 +1,19 @@
+card.onReady = function () {
+    var $card = $('#' + locals.card.id),
+		isActive = true,
+		now = new Date(),
+		renderUTC = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds()).valueOf();
+	if (typeof __e4fcreateben == 'undefined') {		
+		card.require('http://www.exchange4free.com/xandgo/lib/createbeneficiary.js?v=' + Math.round(new Date().getTime() / 1000), function () {
+			console.log('Finding');
+			init();
+		});
+	}
+	else {
+		init();
+	}
+	function init() {
+		__e4fcreateben.createbeneficiary.onReady(card, $card, locals);
+	}
+
+}
